d921207ffa28b340a9c9c73674a9de09283d4a4a
cornicheli <gabrielcornide.pg@gmail.com>
2023-08-02 08:26:18 -0300

    fresh-fetching-paused-stale-inactive

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,31 @@
+import { githubApi } from '../../api/githubApi';
+import { useQuery } from "@tanstack/react-query"
+import { Label } from "../interfaces/label";
+import { sleep } from '../../helpers/sleep';
+
+
+const getLabels = async (): Promise<Label[]> => {
+
+    await sleep(2)
+
+    const { data } = await githubApi.get<Label[]>('/labels')
+    return data
+
+};
+
+export const useLabels = () => {
+
+    const labelsQuery = useQuery(
+        ['labels'],
+        getLabels,
+        {
+            staleTime: 1000 * 60 * 60
+        }
+        // {
+        //     evita el refresh/carga de datos al cambiar de ventana/pantalla
+        //     refetchOnWindowFocus: false
+        // }
+    )
+
+    return labelsQuery
+}
